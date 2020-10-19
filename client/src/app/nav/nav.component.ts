49403ad66867cbb5519fcf16edf99c74e49c7576
khoaNguyen2pt0 <var.khoaNguyen@gmail.com>
2020-10-19 00:13:05 -0300

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,19 +21,12 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-      console.log(response);
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
 
   logout() {
     this.accountService.logout();
     this.router.navigateByUrl('/');
-
   }
-
-
 
 }
