6561273c404d43d00f03ab81508da2c30f80947f
khoaNguyen2pt0 <var.khoaNguyen@gmail.com>
2020-10-22 17:48:15 -0300

    End of Section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+
+        private void ForMember(Func<object, object> p1, Func<object, object> p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
