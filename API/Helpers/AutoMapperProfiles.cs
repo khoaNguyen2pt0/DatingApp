0833dcdd894dc937429b556c9bc171c52a9f237d
khoaNguyen2pt0 <var.khoaNguyen@gmail.com>
2020-10-30 10:57:21 -0300

    End of Section 9

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -16,6 +16,7 @@ namespace API.Helpers
                 .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
 
         private void ForMember(Func<object, object> p1, Func<object, object> p2)
